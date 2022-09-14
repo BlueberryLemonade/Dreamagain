f57b1674f3f0013d86d9bc2fb0b3bc5b105fb5d4
Justin <Justin.Swatloski@gmail.com>
2022-09-13 22:19:55 -0500

    Refactoring out the save file method

--- src/main/java/Dream.java
+++ src/main/java/Dream.java
@@ -2,21 +2,21 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 
 public class Dream {
     private static FileWriter file;
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("LLLL dd yy");
+    LocalDateTime now = LocalDateTime.now();
+    JSONObject test = new JSONObject();
 
-    public static void main(String[] args) {
 
-
-
-        JSONObject test = new JSONObject();
-        test.put("1", "One");
-        System.out.println(test);
+    public void SaveFile(JSONObject fileTBS){
 
         try{
             file = new FileWriter("C:/Development/DreamSample/Dream2.txt");
-            file.write(test.toString());
+            file.write(fileTBS.toString());
             System.out.println("Saved JSON");
         } catch (IOException e){
             e.printStackTrace();
@@ -28,5 +28,23 @@ public class Dream {
                 e.printStackTrace();
             }
         }
+
     }
+
+    public void PrepareJSON(){
+        System.out.println(formatter.format(now));
+        test.put("Date", formatter.format(now));
+        test.put("Text", "Dream");
+        test.put("Third", "3");
+        System.out.println(test);
+        SaveFile(test);
+    }
+
+    public static void main(String[] args) {
+        Dream dream = new Dream();
+        dream.PrepareJSON();
+
+    }
+
+
 }
