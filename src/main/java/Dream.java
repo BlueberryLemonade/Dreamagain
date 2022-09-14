e6793f7b90b781c7dea3d5abd320167b888b5f20
Justin <Justin.Swatloski@gmail.com>
2022-09-13 21:49:20 -0500

    Created a JSON file

--- src/main/java/Dream.java
+++ src/main/java/Dream.java
@@ -4,10 +4,29 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Dream {
+    private static FileWriter file;
+
     public static void main(String[] args) {
+
+
 
         JSONObject test = new JSONObject();
         test.put("1", "One");
         System.out.println(test);
+
+        try{
+            file = new FileWriter("C:/Development/DreamSample/Dream2.txt");
+            file.write(test.toString());
+            System.out.println("Saved JSON");
+        } catch (IOException e){
+            e.printStackTrace();
+        } finally {
+            try {
+                file.flush();
+                file.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
