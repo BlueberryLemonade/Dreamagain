284669111136a129d698bb9d6b95bacb8481db2c
Justin <Justin.Swatloski@gmail.com>
2022-09-13 20:06:38 -0500

    Initial commit, working JSON object creation

---
+++ src/main/java/Dream.java
@@ -0,0 +1,13 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Dream {
+    public static void main(String[] args) {
+
+        JSONObject test = new JSONObject();
+        test.put("1", "One");
+        System.out.println(test);
+    }
+}
